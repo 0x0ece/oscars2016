fc6652222d64d5ca3456fedb865de6039a38f2b3
l.albertalli <l.albertalli@gmail.com>
2016-02-27 00:04:54 -0800

    Running on Google CloudDataflow

--- gc-dataflow-twitter/src/main/java/com/shopkick/data/dataflow/TwitterDataflow.java
+++ gc-dataflow-twitter/src/main/java/com/shopkick/data/dataflow/TwitterDataflow.java
@@ -1,13 +1,7 @@
 package com.shopkick.data.dataflow;
 
-import com.dataartisans.flink.dataflow.FlinkPipelineRunner;
-import com.dataartisans.flink.dataflow.translation.wrappers.streaming.io.UnboundedFlinkSource;
-import com.dataartisans.flink.dataflow.FlinkPipelineOptions;
-import com.dataartisans.flink.dataflow.io.KafkaIO;
-
 import com.google.cloud.dataflow.sdk.Pipeline;
-import com.google.cloud.dataflow.sdk.io.Read;
-import com.google.cloud.dataflow.sdk.io.TextIO;
+import com.google.cloud.dataflow.sdk.io.PubsubIO;
 import com.google.cloud.dataflow.sdk.io.UnboundedSource;
 import com.google.cloud.dataflow.sdk.options.DataflowPipelineOptions;
 import com.google.cloud.dataflow.sdk.options.Default;
@@ -31,11 +25,6 @@ import com.google.cloud.dataflow.sdk.values.KV;
 import com.google.cloud.dataflow.sdk.values.PCollection;
 import com.google.cloud.dataflow.sdk.values.PDone;
 
-import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer082;
-import org.apache.flink.streaming.connectors.kafka.FlinkKafkaProducer;
-import org.apache.flink.streaming.util.serialization.SimpleStringSchema;
-import org.apache.flink.streaming.util.serialization.JavaDefaultStringSchema;
-
 import java.util.Properties;
 
 import org.json.JSONObject;
@@ -52,14 +41,12 @@ import org.slf4j.LoggerFactory;
  */
 public class TwitterDataflow {
 
-  static final String INPUT = "tweets";  // Default kafka topic to read from
-  static final String OUTPUT = "entities_count";  // Default kafka topic to read from
-  static final String ZOOKEEPER = "localhost:2181";  // Default zookeeper to connect to for Kafka
-  static final String KAFKA_BROKER = "localhost:9092";  // Default kafka broker to contact
-  static final String KAFKA_GROUP_ID = "tweetsGroup";  // Default groupId
   static final long WINDOW_SIZE = 600;  // Default window duration in seconds
   static final long WINDOW_SLIDE = 10;  // Default window slide in seconds
 
+  static final String INPUT = "/topics/theneeds0/twstream";  // Default kafka topic to read from
+  static final String OUTPUT = "/topics/theneeds0/twout";  // Default kafka topic to read from
+  
   /** Extracts entities (hashtags and mentions) from a tweet.
       Input is a json string.
       Supports quoted_status, emitted with the quoting tweet timestamp.
@@ -116,22 +103,10 @@ public class TwitterDataflow {
       comma separated string: window end, entity, count.
       TODO: change to SimpleFunction when support for Dataflow 1.4 is available.
   */
-  public static class FormatAsTextFn extends DoFn<KV<String, Long>, String> {
+  public static class FormatAsTextFn extends DoFn<KV<String, Long>, String> implements DoFn.RequiresWindowAccess{
     @Override
     public void processElement(ProcessContext c) {
       String text = ((IntervalWindow)c.window()).end() + "," + c.element().getKey() + "," + c.element().getValue();
-      c.output(text);
-    }
-  }
-
-  /** Converts a globally windowed (Entity, Count) into a printable
-      comma separated string: entity, count.  
-      TODO: change to SimpleFunction when support for Dataflow 1.4 is available.
-  */
-  public static class FormatAsTextGlobalFn extends DoFn<KV<String, Long>, String> {
-    @Override
-    public void processElement(ProcessContext c) {
-      String text = c.element().getKey() + "," + c.element().getValue();
       c.output(text);
     }
   }
@@ -140,34 +115,16 @@ public class TwitterDataflow {
    * Options supported by {@link TwitterDataflow}.
    * Inherits standard configuration options, and flink-dataflow options.
    */
-  public static interface TwitterDataflowOptions extends PipelineOptions, FlinkPipelineOptions {
-    @Description("Kafka topic to read from")
+  public static interface TwitterDataflowOptions extends PipelineOptions, DataflowPipelineOptions {
+    @Description("Pubsub topic to read from")
     @Default.String(INPUT)
     String getInput();
     void setInput(String value);
 
-    @Description("Kafka topic to write to (or, path of the file to write to)")
+    @Description("Pubsub topic to write to")
     @Default.String(OUTPUT)
     String getOutput();
     void setOutput(String value);
-
-    @Description("The Zookeeper server to connect to")
-    @Default.String(ZOOKEEPER)
-    String getZookeeper();
-
-    void setZookeeper(String value);
-
-    @Description("The Kafka Broker to read from/write to")
-    @Default.String(KAFKA_BROKER)
-    String getKafkaBroker();
-
-    void setKafkaBroker(String value);
-
-    @Description("The groupId")
-    @Default.String(KAFKA_GROUP_ID)
-    String getKafkaGroup();
-
-    void setKafkaGroup(String value);
 
     @Description("Sliding window duration, in seconds")
     @Default.Long(WINDOW_SIZE)
@@ -189,37 +146,15 @@ public class TwitterDataflow {
     TwitterDataflowOptions options = PipelineOptionsFactory.fromArgs(args).withValidation()
       .as(TwitterDataflowOptions.class);
 
-    // Flink specific options
+    // Cloud Dataflow specific options
     options.setJobName("TwitterDataflow");
     options.setStreaming(true);
-    options.setRunner(FlinkPipelineRunner.class);
-
+    options.setMaxNumWorkers(4);
+    
     Pipeline pipeline = Pipeline.create(options);
 
-    // Kafka options, shared among consumer and producer
-    Properties kafkaProps = new Properties();
-    kafkaProps.setProperty("zookeeper.connect", options.getZookeeper());
-    kafkaProps.setProperty("bootstrap.servers", options.getKafkaBroker());
-    kafkaProps.setProperty("group.id", options.getKafkaGroup());
-
-    // Flink consumer that reads from a Kafka topic
-    FlinkKafkaConsumer082 kafkaConsumer = new FlinkKafkaConsumer082<String>(
-      options.getInput(),
-      new SimpleStringSchema(), kafkaProps);
-
-    // Flink producer that writes to a Kafka topic
-    FlinkKafkaProducer<String> kafkaProducer = new FlinkKafkaProducer<String>(
-      options.getOutput(),
-      new JavaDefaultStringSchema(), kafkaProps, null);
-
-    // This requires KafkaIO, check out https://github.com/ecesena/flink-dataflow
-    // or use TextIO as in the comment below
-    PTransform<PCollection<String>, PDone> outputFn =
-        new KafkaIO.Write.Bound<String>("WriteEntityCounts", kafkaProducer);
-        // TextIO.Write.named("WriteEntityCounts").to(options.getOutput());
-
     pipeline
-      .apply("Input", Read.named("ReadTweets").from(new UnboundedFlinkSource<String, UnboundedSource.CheckpointMark>(options, kafkaConsumer)).named("StreamingTweets"))
+      .apply("Input", PubsubIO.Read.topic(options.getInput()))
       .apply("Extract", ParDo.of(new ExtractEntitiesFn()))
       .apply("Window", Window.<String>into(SlidingWindows.of(Duration.standardSeconds(options.getWindowSize()))
             .every(Duration.standardSeconds(options.getWindowSlide()))
@@ -229,7 +164,7 @@ public class TwitterDataflow {
         )
       .apply("Count", Count.<String>perElement())
       .apply("Format", ParDo.of(new FormatAsTextFn()))
-      .apply("Output", outputFn)
+      .apply("Output", PubsubIO.Write.topic(options.getOutput()))
       ;
 
     pipeline.run();
